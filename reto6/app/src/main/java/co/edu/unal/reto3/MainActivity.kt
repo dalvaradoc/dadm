10109dde43cc10276bd8bac3aac7e0ccabe8bc8d
dalvaradoc <dalvaradoc@unal.edu.co>
2023-10-10 20:52:53 -0500

    reto6 added

---
+++ reto6/app/src/main/java/co/edu/unal/reto3/MainActivity.kt
@@ -0,0 +1,200 @@
+package co.edu.unal.reto3
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.MotionEvent
+import android.view.View.OnTouchListener
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
+
+
+class MainActivity : AppCompatActivity() {
+    val mGame: TicTacToeGame = TicTacToeGame()
+
+    private lateinit var mInfoTextView : TextView
+    private lateinit var mBoardView : BoardView
+    private lateinit var mHumanMediaPlayer : MediaPlayer
+    private lateinit var mComputerMediaPlayer : MediaPlayer
+
+    private var mGameOver : Boolean = false
+    private var mPlayerTurn : Char = TicTacToeGame.HUMAN_PLAYER
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layout)
+
+        mBoardView = findViewById(R.id.board)
+        mBoardView.setGame(mGame)
+        mBoardView.setOnTouchListener(mTouchListener)
+
+        supportActionBar?.title = "Reto5"
+
+        mInfoTextView = findViewById<TextView>(R.id.information)
+
+        startNewGame()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        mHumanMediaPlayer = MediaPlayer.create(applicationContext, R.raw.human_move)
+        mComputerMediaPlayer = MediaPlayer.create(applicationContext, R.raw.computer_move)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        mHumanMediaPlayer.release()
+        mComputerMediaPlayer.release()
+    }
+
+    private fun startNewGame() {
+        mGame.clearBoard()
+        mGameOver = false
+        mBoardView.invalidate()
+        mInfoTextView.setText(R.string.first_human)
+    }
+
+    private fun setMove(player: Char, location : Int) : Boolean {
+        if (mGame.setMove(player, location)){
+            mBoardView.invalidate()
+            return true
+        }
+        return false
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+
+        var inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.options_menu, menu)
+
+        return true
+    }
+
+    class DifficultyDialogFragment : DialogFragment() {
+
+        private var mainActivity : MainActivity? = null
+
+        fun setMainActivity(activity: MainActivity) {
+            mainActivity = activity
+        }
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+            return activity?.let {
+                var currentLevel = mainActivity?.mGame?.difficultyLevelInt
+                if (currentLevel == null)
+                    currentLevel = 2
+                val builder = AlertDialog.Builder(it)
+                // Set the dialog title
+                builder.setTitle(R.string.difficulty_choose)
+                    // Specify the list array, the items to be selected by default (null for none),
+                    // and the listener through which to receive callbacks when items are selected
+                    .setSingleChoiceItems(R.array.difficulties, currentLevel,
+                        DialogInterface.OnClickListener() { dialog, which ->
+                            mainActivity?.mGame?.setDifficultyLevel(which)
+                        })
+
+                builder.create()
+            } ?: throw IllegalStateException("Activity cannot be null")
+        }
+    }
+
+    class QuitDialogFragment : DialogFragment() {
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+            return activity?.let {
+                var builder = AlertDialog.Builder(it)
+                builder.setTitle(R.string.quit_question)
+                    .setPositiveButton(R.string.yes, DialogInterface.OnClickListener() { dialogInterface, i ->
+                        activity?.finish()
+                    })
+                    .setNegativeButton(R.string.no, null)
+                builder.create()
+            } ?: throw IllegalStateException("Activity cannot be null")
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.new_game -> {
+                startNewGame()
+                return true
+            }
+            R.id.ai_difficulty -> {
+                val difficultyDialog = DifficultyDialogFragment()
+                difficultyDialog.setMainActivity(this)
+                difficultyDialog.show(supportFragmentManager, "dialog")
+                return true
+            }
+            R.id.quit -> {
+                val quitDialogFragment = QuitDialogFragment()
+                quitDialogFragment.show(supportFragmentManager, "dialog")
+                return true
+            }
+        }
+        return false
+    }
+
+    private val mTouchListener = OnTouchListener {v, event ->
+        v.performClick()
+
+        val col = (event.x / mBoardView.getBoardCellWidth()).toInt();
+        val row = (event.y / mBoardView.getBoardCellHeight()).toInt();
+        val pos = row * 3 + col;
+
+        println(pos)
+
+        if (!mGameOver && mPlayerTurn == TicTacToeGame.HUMAN_PLAYER && setMove(TicTacToeGame.HUMAN_PLAYER, pos)) {
+            mHumanMediaPlayer.start()
+
+            var winner = mGame.checkForWinner()
+            if (winner == 0){
+                mPlayerTurn = TicTacToeGame.COMPUTER_PLAYER
+                mInfoTextView.setText(R.string.turn_computer)
+                Handler(Looper.getMainLooper()).postDelayed({
+                    val move = mGame.computerMove
+                    setMove(TicTacToeGame.COMPUTER_PLAYER, move)
+                    mComputerMediaPlayer.start()
+                    mPlayerTurn = TicTacToeGame.HUMAN_PLAYER
+                    winner = mGame.checkForWinner()
+
+                    if (winner != 0)
+                        mGameOver = true
+
+                    when (winner) {
+                        0 -> mInfoTextView.setText(R.string.turn_human)
+                        1 -> mInfoTextView.setText(R.string.result_tie)
+                        2 -> mInfoTextView.setText(R.string.result_human_wins)
+                        3 -> mInfoTextView.setText(R.string.result_computer_wins)
+                        else -> mInfoTextView.text = "Error D:"
+                    }
+                }, 1000)
+            }
+
+            if (winner != 0)
+                mGameOver = true
+
+            when (winner) {
+                0 -> mInfoTextView.setText(R.string.turn_computer)
+                1 -> mInfoTextView.setText(R.string.result_tie)
+                2 -> mInfoTextView.setText(R.string.result_human_wins)
+                3 -> mInfoTextView.setText(R.string.result_computer_wins)
+                else -> mInfoTextView.text = "Error D:"
+            }
+        }
+
+        false
+    }
+
+    fun main() {
+        startNewGame()
+    }
+}
---
+++ reto6/app/src/main/java/co/edu/unal/reto3/TicTacToeGame.java
@@ -0,0 +1,175 @@
+package co.edu.unal.reto3;
+
+/* TicTacToeConsole.java
+ * By Frank McCown (Harding University)
+ *
+ * This is a tic-tac-toe game that runs in the console window.  The human
+ * is X and the computer is O.
+ */
+
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class TicTacToeGame {
+    public final static int BOARD_SIZE = 9;
+    private char mBoard[] = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
+    public static final char HUMAN_PLAYER = 'X';
+    public static final char COMPUTER_PLAYER = 'O';
+    public static final char OPEN_SPOT = ' ';
+    // The computer's difficulty levels
+    public enum DifficultyLevel {Easy, Harder, Expert};
+    // Current difficulty level
+    private DifficultyLevel mDifficultyLevel = DifficultyLevel.Expert;
+
+    private Random mRand;
+
+
+    public TicTacToeGame() {
+
+        // Seed the random number generator
+        mRand = new Random();
+    }
+
+    // Check for a winner.  Return
+    //  0 if no winner or tie yet
+    //  1 if it's a tie
+    //  2 if X won
+    //  3 if O won
+    public int checkForWinner() {
+
+        // Check horizontal wins
+        for (int i = 0; i <= 6; i += 3)	{
+            if (mBoard[i] == HUMAN_PLAYER &&
+                    mBoard[i+1] == HUMAN_PLAYER &&
+                    mBoard[i+2]== HUMAN_PLAYER)
+                return 2;
+            if (mBoard[i] == COMPUTER_PLAYER &&
+                    mBoard[i+1]== COMPUTER_PLAYER &&
+                    mBoard[i+2] == COMPUTER_PLAYER)
+                return 3;
+        }
+
+        // Check vertical wins
+        for (int i = 0; i <= 2; i++) {
+            if (mBoard[i] == HUMAN_PLAYER &&
+                    mBoard[i+3] == HUMAN_PLAYER &&
+                    mBoard[i+6]== HUMAN_PLAYER)
+                return 2;
+            if (mBoard[i] == COMPUTER_PLAYER &&
+                    mBoard[i+3] == COMPUTER_PLAYER &&
+                    mBoard[i+6]== COMPUTER_PLAYER)
+                return 3;
+        }
+
+        // Check for diagonal wins
+        if ((mBoard[0] == HUMAN_PLAYER &&
+                mBoard[4] == HUMAN_PLAYER &&
+                mBoard[8] == HUMAN_PLAYER) ||
+                (mBoard[2] == HUMAN_PLAYER &&
+                        mBoard[4] == HUMAN_PLAYER &&
+                        mBoard[6] == HUMAN_PLAYER))
+            return 2;
+        if ((mBoard[0] == COMPUTER_PLAYER &&
+                mBoard[4] == COMPUTER_PLAYER &&
+                mBoard[8] == COMPUTER_PLAYER) ||
+                (mBoard[2] == COMPUTER_PLAYER &&
+                        mBoard[4] == COMPUTER_PLAYER &&
+                        mBoard[6] == COMPUTER_PLAYER))
+            return 3;
+
+        // Check for tie
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            // If we find a number, then no one has won yet
+            if (mBoard[i] != HUMAN_PLAYER && mBoard[i] != COMPUTER_PLAYER)
+                return 0;
+        }
+
+        // If we make it through the previous loop, all places are taken, so it's a tie
+        return 1;
+    }
+
+    public int getComputerMove()
+    {
+        int move;
+
+        // Check if difficulty is not easy (so is expert or hard)
+        // First see if there's a move O can make to win
+        if (mDifficultyLevel != DifficultyLevel.Easy) {
+            for (int i = 0; i < BOARD_SIZE; i++) {
+                if (mBoard[i] == OPEN_SPOT) {
+                    mBoard[i] = COMPUTER_PLAYER;
+                    if (checkForWinner() == 3) {
+                        mBoard[i] = OPEN_SPOT;
+                        return i;
+                    }
+                    mBoard[i] = OPEN_SPOT;
+                }
+            }
+        }
+
+        // Check if difficulty is expert
+        // See if there's a move O can make to block X from winning
+        if (mDifficultyLevel == DifficultyLevel.Expert) {
+            for (int i = 0; i < BOARD_SIZE; i++) {
+                if (mBoard[i] == OPEN_SPOT) {
+                    mBoard[i] = HUMAN_PLAYER;
+                    if (checkForWinner() == 2) {
+                        mBoard[i] = OPEN_SPOT;
+                        return i;
+                    }
+                    mBoard[i] = OPEN_SPOT;
+                }
+            }
+        }
+
+        // Any difficulty can end up in a random move
+        // Generate random move
+        do
+        {
+            move = mRand.nextInt(BOARD_SIZE);
+        } while (mBoard[move] == HUMAN_PLAYER || mBoard[move] == COMPUTER_PLAYER);
+
+        return move;
+    }
+
+    public void clearBoard() {
+        for (int i = 0; i < BOARD_SIZE; ++i){
+            mBoard[i] = OPEN_SPOT;
+        }
+    }
+
+    public boolean setMove(char player, int location){
+        if (player != HUMAN_PLAYER && player != COMPUTER_PLAYER){
+            System.out.println("Error, player not valid.");
+            return false;
+        }
+
+        if (location < 0 || location > BOARD_SIZE-1 || mBoard[location] != OPEN_SPOT) {
+            System.out.println("Error, location no valid");
+            return false;
+        }
+
+        mBoard[location] = player;
+        System.out.println(player + " placed on " + location);
+        return true;
+    }
+
+    public DifficultyLevel getDifficultyLevel() {
+        return mDifficultyLevel;
+    }
+    public void setDifficultyLevel(int difficultyLevel) {
+        if (difficultyLevel < 0 || difficultyLevel > 2){
+            return;
+        }
+        mDifficultyLevel = DifficultyLevel.values()[difficultyLevel];
+    }
+
+    public int getDifficultyLevelInt () {
+        return mDifficultyLevel.ordinal();
+    }
+
+    public char getBoardOccupant (int i){
+        return mBoard[i];
+    }
+}
